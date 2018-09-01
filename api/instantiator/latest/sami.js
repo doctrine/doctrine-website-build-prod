3699eb00af41f987c901600e095345a5125ff1ba
Doctrine Deployer <deployer@doctrine-project.org>
2018-09-01 12:26:43 +0000

    New version of Doctrine website

---
+++ api/instantiator/latest/sami.js
@@ -0,0 +1,217 @@
+
+window.projectVersion = 'master';
+
+(function(root) {
+
+    var bhIndex = null;
+    var rootPath = '';
+    var treeHtml = '        <ul>                <li data-name="namespace:Doctrine" class="opened">                    <div style="padding-left:0px" class="hd">                        <span class="glyphicon glyphicon-play"></span><a href="Doctrine.html">Doctrine</a>                    </div>                    <div class="bd">                                <ul>                <li data-name="namespace:Doctrine_Instantiator" class="opened">                    <div style="padding-left:18px" class="hd">                        <span class="glyphicon glyphicon-play"></span><a href="Doctrine/Instantiator.html">Instantiator</a>                    </div>                    <div class="bd">                                <ul>                <li data-name="namespace:Doctrine_Instantiator_Exception" >                    <div style="padding-left:36px" class="hd">                        <span class="glyphicon glyphicon-play"></span><a href="Doctrine/Instantiator/Exception.html">Exception</a>                    </div>                    <div class="bd">                                <ul>                <li data-name="class:Doctrine_Instantiator_Exception_ExceptionInterface" >                    <div style="padding-left:62px" class="hd leaf">                        <a href="Doctrine/Instantiator/Exception/ExceptionInterface.html">ExceptionInterface</a>                    </div>                </li>                            <li data-name="class:Doctrine_Instantiator_Exception_InvalidArgumentException" >                    <div style="padding-left:62px" class="hd leaf">                        <a href="Doctrine/Instantiator/Exception/InvalidArgumentException.html">InvalidArgumentException</a>                    </div>                </li>                            <li data-name="class:Doctrine_Instantiator_Exception_UnexpectedValueException" >                    <div style="padding-left:62px" class="hd leaf">                        <a href="Doctrine/Instantiator/Exception/UnexpectedValueException.html">UnexpectedValueException</a>                    </div>                </li>                </ul></div>                </li>                            <li data-name="class:Doctrine_Instantiator_Instantiator" >                    <div style="padding-left:44px" class="hd leaf">                        <a href="Doctrine/Instantiator/Instantiator.html">Instantiator</a>                    </div>                </li>                            <li data-name="class:Doctrine_Instantiator_InstantiatorInterface" >                    <div style="padding-left:44px" class="hd leaf">                        <a href="Doctrine/Instantiator/InstantiatorInterface.html">InstantiatorInterface</a>                    </div>                </li>                </ul></div>                </li>                </ul></div>                </li>                </ul>';
+
+    var searchTypeClasses = {
+        'Namespace': 'label-default',
+        'Class': 'label-info',
+        'Interface': 'label-primary',
+        'Trait': 'label-success',
+        'Method': 'label-danger',
+        '_': 'label-warning'
+    };
+
+    var searchIndex = [
+                    
+            {"type": "Namespace", "link": "Doctrine.html", "name": "Doctrine", "doc": "Namespace Doctrine"},{"type": "Namespace", "link": "Doctrine/Instantiator.html", "name": "Doctrine\\Instantiator", "doc": "Namespace Doctrine\\Instantiator"},{"type": "Namespace", "link": "Doctrine/Instantiator/Exception.html", "name": "Doctrine\\Instantiator\\Exception", "doc": "Namespace Doctrine\\Instantiator\\Exception"},
+            {"type": "Interface", "fromName": "Doctrine\\Instantiator\\Exception", "fromLink": "Doctrine/Instantiator/Exception.html", "link": "Doctrine/Instantiator/Exception/ExceptionInterface.html", "name": "Doctrine\\Instantiator\\Exception\\ExceptionInterface", "doc": "&quot;Base exception marker interface for the instantiator component&quot;"},
+                    
+            {"type": "Interface", "fromName": "Doctrine\\Instantiator", "fromLink": "Doctrine/Instantiator.html", "link": "Doctrine/Instantiator/InstantiatorInterface.html", "name": "Doctrine\\Instantiator\\InstantiatorInterface", "doc": "&quot;Instantiator provides utility methods to build objects without invoking their constructors&quot;"},
+                                                        {"type": "Method", "fromName": "Doctrine\\Instantiator\\InstantiatorInterface", "fromLink": "Doctrine/Instantiator/InstantiatorInterface.html", "link": "Doctrine/Instantiator/InstantiatorInterface.html#method_instantiate", "name": "Doctrine\\Instantiator\\InstantiatorInterface::instantiate", "doc": "&quot;&quot;"},
+            
+            
+            {"type": "Class", "fromName": "Doctrine\\Instantiator\\Exception", "fromLink": "Doctrine/Instantiator/Exception.html", "link": "Doctrine/Instantiator/Exception/ExceptionInterface.html", "name": "Doctrine\\Instantiator\\Exception\\ExceptionInterface", "doc": "&quot;Base exception marker interface for the instantiator component&quot;"},
+                    
+            {"type": "Class", "fromName": "Doctrine\\Instantiator\\Exception", "fromLink": "Doctrine/Instantiator/Exception.html", "link": "Doctrine/Instantiator/Exception/InvalidArgumentException.html", "name": "Doctrine\\Instantiator\\Exception\\InvalidArgumentException", "doc": "&quot;Exception for invalid arguments provided to the instantiator&quot;"},
+                                                        {"type": "Method", "fromName": "Doctrine\\Instantiator\\Exception\\InvalidArgumentException", "fromLink": "Doctrine/Instantiator/Exception/InvalidArgumentException.html", "link": "Doctrine/Instantiator/Exception/InvalidArgumentException.html#method_fromNonExistingClass", "name": "Doctrine\\Instantiator\\Exception\\InvalidArgumentException::fromNonExistingClass", "doc": "&quot;&quot;"},
+                    {"type": "Method", "fromName": "Doctrine\\Instantiator\\Exception\\InvalidArgumentException", "fromLink": "Doctrine/Instantiator/Exception/InvalidArgumentException.html", "link": "Doctrine/Instantiator/Exception/InvalidArgumentException.html#method_fromAbstractClass", "name": "Doctrine\\Instantiator\\Exception\\InvalidArgumentException::fromAbstractClass", "doc": "&quot;&quot;"},
+            
+            {"type": "Class", "fromName": "Doctrine\\Instantiator\\Exception", "fromLink": "Doctrine/Instantiator/Exception.html", "link": "Doctrine/Instantiator/Exception/UnexpectedValueException.html", "name": "Doctrine\\Instantiator\\Exception\\UnexpectedValueException", "doc": "&quot;Exception for given parameters causing invalid\/unexpected state on instantiation&quot;"},
+                                                        {"type": "Method", "fromName": "Doctrine\\Instantiator\\Exception\\UnexpectedValueException", "fromLink": "Doctrine/Instantiator/Exception/UnexpectedValueException.html", "link": "Doctrine/Instantiator/Exception/UnexpectedValueException.html#method_fromSerializationTriggeredException", "name": "Doctrine\\Instantiator\\Exception\\UnexpectedValueException::fromSerializationTriggeredException", "doc": "&quot;&quot;"},
+                    {"type": "Method", "fromName": "Doctrine\\Instantiator\\Exception\\UnexpectedValueException", "fromLink": "Doctrine/Instantiator/Exception/UnexpectedValueException.html", "link": "Doctrine/Instantiator/Exception/UnexpectedValueException.html#method_fromUncleanUnSerialization", "name": "Doctrine\\Instantiator\\Exception\\UnexpectedValueException::fromUncleanUnSerialization", "doc": "&quot;&quot;"},
+            
+            {"type": "Class", "fromName": "Doctrine\\Instantiator", "fromLink": "Doctrine/Instantiator.html", "link": "Doctrine/Instantiator/Instantiator.html", "name": "Doctrine\\Instantiator\\Instantiator", "doc": "&quot;{@inheritDoc}&quot;"},
+                                                        {"type": "Method", "fromName": "Doctrine\\Instantiator\\Instantiator", "fromLink": "Doctrine/Instantiator/Instantiator.html", "link": "Doctrine/Instantiator/Instantiator.html#method_instantiate", "name": "Doctrine\\Instantiator\\Instantiator::instantiate", "doc": "&quot;&quot;"},
+            
+            {"type": "Class", "fromName": "Doctrine\\Instantiator", "fromLink": "Doctrine/Instantiator.html", "link": "Doctrine/Instantiator/InstantiatorInterface.html", "name": "Doctrine\\Instantiator\\InstantiatorInterface", "doc": "&quot;Instantiator provides utility methods to build objects without invoking their constructors&quot;"},
+                                                        {"type": "Method", "fromName": "Doctrine\\Instantiator\\InstantiatorInterface", "fromLink": "Doctrine/Instantiator/InstantiatorInterface.html", "link": "Doctrine/Instantiator/InstantiatorInterface.html#method_instantiate", "name": "Doctrine\\Instantiator\\InstantiatorInterface::instantiate", "doc": "&quot;&quot;"},
+            
+            
+                                        // Fix trailing commas in the index
+        {}
+    ];
+
+    /** Tokenizes strings by namespaces and functions */
+    function tokenizer(term) {
+        if (!term) {
+            return [];
+        }
+
+        var tokens = [term];
+        var meth = term.indexOf('::');
+
+        // Split tokens into methods if "::" is found.
+        if (meth > -1) {
+            tokens.push(term.substr(meth + 2));
+            term = term.substr(0, meth - 2);
+        }
+
+        // Split by namespace or fake namespace.
+        if (term.indexOf('\\') > -1) {
+            tokens = tokens.concat(term.split('\\'));
+        } else if (term.indexOf('_') > 0) {
+            tokens = tokens.concat(term.split('_'));
+        }
+
+        // Merge in splitting the string by case and return
+        tokens = tokens.concat(term.match(/(([A-Z]?[^A-Z]*)|([a-z]?[^a-z]*))/g).slice(0,-1));
+
+        return tokens;
+    };
+
+    root.Sami = {
+        /**
+         * Cleans the provided term. If no term is provided, then one is
+         * grabbed from the query string "search" parameter.
+         */
+        cleanSearchTerm: function(term) {
+            // Grab from the query string
+            if (typeof term === 'undefined') {
+                var name = 'search';
+                var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+                var results = regex.exec(location.search);
+                if (results === null) {
+                    return null;
+                }
+                term = decodeURIComponent(results[1].replace(/\+/g, " "));
+            }
+
+            return term.replace(/<(?:.|\n)*?>/gm, '');
+        },
+
+        /** Searches through the index for a given term */
+        search: function(term) {
+            // Create a new search index if needed
+            if (!bhIndex) {
+                bhIndex = new Bloodhound({
+                    limit: 500,
+                    local: searchIndex,
+                    datumTokenizer: function (d) {
+                        return tokenizer(d.name);
+                    },
+                    queryTokenizer: Bloodhound.tokenizers.whitespace
+                });
+                bhIndex.initialize();
+            }
+
+            results = [];
+            bhIndex.get(term, function(matches) {
+                results = matches;
+            });
+
+            if (!rootPath) {
+                return results;
+            }
+
+            // Fix the element links based on the current page depth.
+            return $.map(results, function(ele) {
+                if (ele.link.indexOf('..') > -1) {
+                    return ele;
+                }
+                ele.link = rootPath + ele.link;
+                if (ele.fromLink) {
+                    ele.fromLink = rootPath + ele.fromLink;
+                }
+                return ele;
+            });
+        },
+
+        /** Get a search class for a specific type */
+        getSearchClass: function(type) {
+            return searchTypeClasses[type] || searchTypeClasses['_'];
+        },
+
+        /** Add the left-nav tree to the site */
+        injectApiTree: function(ele) {
+            ele.html(treeHtml);
+        }
+    };
+
+    $(function() {
+        // Modify the HTML to work correctly based on the current depth
+        rootPath = $('body').attr('data-root-path');
+        treeHtml = treeHtml.replace(/href="/g, 'href="' + rootPath);
+        Sami.injectApiTree($('#api-tree'));
+    });
+
+    return root.Sami;
+})(window);
+
+$(function() {
+
+    // Enable the version switcher
+    $('#version-switcher').change(function() {
+        window.location = $(this).val()
+    });
+
+    
+        // Toggle left-nav divs on click
+        $('#api-tree .hd span').click(function() {
+            $(this).parent().parent().toggleClass('opened');
+        });
+
+        // Expand the parent namespaces of the current page.
+        var expected = $('body').attr('data-name');
+
+        if (expected) {
+            // Open the currently selected node and its parents.
+            var container = $('#api-tree');
+            var node = $('#api-tree li[data-name="' + expected + '"]');
+            // Node might not be found when simulating namespaces
+            if (node.length > 0) {
+                node.addClass('active').addClass('opened');
+                node.parents('li').addClass('opened');
+                var scrollPos = node.offset().top - container.offset().top + container.scrollTop();
+                // Position the item nearer to the top of the screen.
+                scrollPos -= 200;
+                container.scrollTop(scrollPos);
+            }
+        }
+
+    
+    
+        var form = $('#search-form .typeahead');
+        form.typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        }, {
+            name: 'search',
+            displayKey: 'name',
+            source: function (q, cb) {
+                cb(Sami.search(q));
+            }
+        });
+
+        // The selection is direct-linked when the user selects a suggestion.
+        form.on('typeahead:selected', function(e, suggestion) {
+            window.location = suggestion.link;
+        });
+
+        // The form is submitted when the user hits enter.
+        form.keypress(function (e) {
+            if (e.which == 13) {
+                $('#search-form').submit();
+                return true;
+            }
+        });
+
+    
+});
+
+
